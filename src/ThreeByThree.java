b0adcd4ed8986a3391c6347e1a369472ac3af4ba
richaar <arianna.richardson@ssfs.org>
2020-03-09 14:14:35 -0400

    Started 3x3

---
+++ src/ThreeByThree.java
@@ -0,0 +1,45 @@
+
+    public class ThreeByThree {
+        public static void main(String [] args){
+
+//code to define an initially empty double
+//2D array size 3x3
+
+            double [][] list = new double[3][3];
+
+//code to output all original values
+
+            System.out.println(list[0][0]);
+            System.out.println(list[0][1]);
+            System.out.println(list[0][2]);
+            System.out.println(list[0][3]);
+            System.out.println(list[0][4]);
+
+            System.out.println(list[1][0]);
+            System.out.println(list[1][1]);
+            System.out.println(list[1][2]);
+            System.out.println(list[1][3]);
+            System.out.println(list[1][4]);
+
+            System.out.println(list[2][0]);
+            System.out.println(list[2][1]);
+            System.out.println(list[2][2]);
+            System.out.println(list[2][3]);
+            System.out.println(list[2][4]);
+
+//code to change five of the values to positive
+
+
+//and the other four to negative
+
+
+//code to output a blank line separating output sets
+
+
+//code to output all array elements
+        }
+    }
+
+
+
+}
